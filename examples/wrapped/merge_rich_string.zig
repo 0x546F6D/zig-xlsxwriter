c9e1db448e6d08fffd6d5ba0ecdadc6925386c46
Tom <0x546f6d@protonmail.com>
2025-05-23 17:09:38 +0300

    wrapped merge_rich_string example

--- examples/wrapped/merge_rich_string.zig
+++ examples/wrapped/merge_rich_string.zig
@@ -1,52 +1,47 @@
-//
-// An example of merging cells containing a rich string using libxlsxwriter.
-//
-// Copyright 2014-2025, John McNamara, jmcnamara@cpan.org
-//
-//
-
-const std = @import("std");
-const xlsxwriter = @import("xlsxwriter");
-
 pub fn main() !void {
-    const workbook = xlsxwriter.workbook_new("zig-merge_rich_string.xlsx");
-    const worksheet = xlsxwriter.workbook_add_worksheet(workbook, null);
+    defer _ = dbga.deinit();
+
+    const xlsx_path = try h.getXlsxPath(alloc, @src().file);
+    defer alloc.free(xlsx_path);
+
+    // Create a workbook and add worksheets.
+    const workbook = try xlsxwriter.initWorkBook(xlsx_path.ptr);
+    defer workbook.deinit() catch {};
+
+    const worksheet = try workbook.addWorkSheet(null);
 
     // Configure a format for the merged range.
-    const merge_format = xlsxwriter.workbook_add_format(workbook);
-    _ = xlsxwriter.format_set_align(merge_format, xlsxwriter.LXW_ALIGN_CENTER);
-    _ = xlsxwriter.format_set_align(
-        merge_format,
-        xlsxwriter.LXW_ALIGN_VERTICAL_CENTER,
-    );
-    _ = xlsxwriter.format_set_border(merge_format, xlsxwriter.LXW_BORDER_THIN);
+    const merge_format = try workbook.addFormat();
+    merge_format.setAlign(.center);
+    merge_format.setAlign(.vertical_center);
+    merge_format.setBorder(.thin);
 
     // Configure formats for the rich string.
-    const red = xlsxwriter.workbook_add_format(workbook);
-    _ = xlsxwriter.format_set_font_color(red, xlsxwriter.LXW_COLOR_RED);
+    const red = try workbook.addFormat();
+    red.setFontColor(.red);
 
-    const blue = xlsxwriter.workbook_add_format(workbook);
-    _ = xlsxwriter.format_set_font_color(blue, xlsxwriter.LXW_COLOR_BLUE);
+    const blue = try workbook.addFormat();
+    blue.setFontColor(.blue);
+    // for comparison with original
+    blue.setFontColor(@enumFromInt(0x0000FF));
 
     // Create the fragments for the rich string.
-    var fragment1 = xlsxwriter.lxw_rich_string_tuple{
-        .format = null,
+    const fragment1: xlsxwriter.RichStringTuple = .{
         .string = "This is ",
     };
-    var fragment2 = xlsxwriter.lxw_rich_string_tuple{
-        .format = red,
+    const fragment2: xlsxwriter.RichStringTuple = .{
+        .format_c = red.format_c,
         .string = "red",
     };
-    var fragment3 = xlsxwriter.lxw_rich_string_tuple{
-        .format = null,
+    const fragment3: xlsxwriter.RichStringTuple = .{
         .string = " and this is ",
     };
-    var fragment4 = xlsxwriter.lxw_rich_string_tuple{
-        .format = blue,
+    const fragment4: xlsxwriter.RichStringTuple = .{
+        .format_c = blue.format_c,
         .string = "blue",
     };
 
-    var rich_string = [_:null]?*xlsxwriter.lxw_rich_string_tuple{
+    const rich_string: xlsxwriter.RichStringType = &.{
         &fragment1,
         &fragment2,
         &fragment3,
@@ -55,26 +50,17 @@ pub fn main() !void {
     };
 
     // Write an empty string to the merged range.
-    _ = xlsxwriter.worksheet_merge_range(
-        worksheet,
-        1,
-        1,
-        4,
-        3,
-        "",
-        merge_format,
-    );
+    try worksheet.mergeRange(1, 1, 4, 4, "", merge_format);
 
     // We then overwrite the first merged cell with a rich string. Note that
     // we must also pass the cell format used in the merged cells format at
     // the end.
-    _ = xlsxwriter.worksheet_write_rich_string(
-        worksheet,
-        1,
-        1,
-        &rich_string,
-        merge_format,
-    );
-
-    _ = xlsxwriter.workbook_close(workbook);
+    try worksheet.writeRichString(1, 1, rich_string, merge_format);
 }
+
+var dbga: @import("std").heap.DebugAllocator(.{}) = .init;
+const alloc = dbga.allocator();
+const h = @import("_helper.zig");
+const xlsxwriter = @import("xlsxwriter");
+const WorkSheet = @import("xlsxwriter").WorkSheet;
+const Format = @import("xlsxwriter").Format;
